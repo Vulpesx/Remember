08a6a128b2855273058cf6981fc3d775086751fe
Vulpesx <potaytochipgamer@gmail.com>
2022-05-18 14:36:17 +1000

    re organised code

---
+++ src/lib.rs
@@ -0,0 +1,185 @@
+use std::str::FromStr;
+
+use libnotify::Notification;
+use chrono::{DateTime, Local, Datelike, Timelike};
+
+#[derive(Debug)]
+pub struct Reminder<'a> {
+    summary: &'a str,
+    body: Option<&'a str>,
+    when: When,
+    done: bool,
+    notif: Notification,
+}
+
+#[derive(Debug, Clone)]
+pub struct Time {
+    hour: u32,
+    minutes: u32,
+}
+
+impl Time {
+    pub fn new(hour: u32, minutes: u32) -> Time {
+        Time { hour, minutes }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Date {
+    day: u32,
+    month: u32,
+    year: i32,
+}
+
+impl Date {
+    pub fn new(day: u32, month: u32, year: i32) -> Date {
+        Date { day, month, year }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum When {
+    Duration(u32),
+    Day(String, Time),
+    Date(Date, Time),
+    Time(Time),
+}
+
+impl<'a> Reminder<'a> {
+    pub fn new<'s>(when: When, summary: &'s str, body: Option<&'s str>) -> Reminder<'s> {
+        Reminder {
+            when,
+            summary,
+            body,
+            notif: Notification::new(summary, body, None),
+            done: false,
+        }
+    }
+
+    pub fn check(&mut self, now: DateTime<Local>) -> bool {
+        use When::*;
+        match self.when.clone() {
+            Duration(s) => {
+                if s > 0 {
+                    self.when = When::Duration(s - 1);
+                    false
+                } else {
+                    true
+                }
+            },
+            Day(d, t) => {
+                if d.to_lowercase() != now.weekday().to_string().to_lowercase() {
+                    return false;
+                }
+                if now.hour() < t.hour {
+                    return false;
+                }
+                if now.minute() < t.minutes {
+                    return false;
+                }
+                true
+            }
+            Date(d, t) => {
+                if now.year() < d.year && d.year != 0 {
+                    return false;
+                }
+                if now.month() < d.month {
+                    return false;
+                }
+                if now.day() < d.day {
+                    return false;
+                }
+                if now.hour() < t.hour {
+                    return false;
+                }
+                if now.minute() < t.minutes {
+                    return false;
+                }
+                true
+            },
+            Time(t) => {
+                if now.hour() < t.hour {
+                    return false;
+                }
+                if now.minute() < t.minutes {
+                    return false;
+                }
+                true
+            }
+        }
+    }
+
+    pub fn show(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        println!("{:?}", self);
+        self.done = true;
+        Ok(self.notif.show()?)
+    }
+
+    pub fn is_done(&self) -> bool {
+        self.done
+    }
+}
+
+#[derive(Debug)]
+pub enum ReminderError<'a> {
+    FromStr(&'a str),
+}
+
+
+impl<'a> FromStr for Reminder<'a> {
+    type Err = ReminderError<'a>;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let v: Vec<&str> = s.split(";").collect();
+        if "montuewedthufrisatsun".contains(v[0]) {
+            todo!("parse days")
+        } else if v[0].contains("/") {
+            todo!("parse dates")
+        } else if v[0].contains(":") {
+            todo!("parse times")
+        } else {
+            todo!("parse durations")
+        }
+
+        Err(ReminderError::FromStr("unknown"))
+    }
+}
+
+#[macro_export]
+macro_rules! remind {
+    ($hour:literal:$minute:literal $sum:literal) => {
+        Reminder::new(When::Time(Time::new($hour, $minute)), $sum, None)
+    };
+    ($hour:literal:$minute:literal $sum:literal $body:literal) => {
+        Reminder::new($crate::When::Time($crate::Time::new($hour, $minute)), $sum, Some($body))
+    };
+    ($day:ident $hour:literal:$minute:literal $sum:literal) => {
+        {
+            let day = stringify!($day).to_lowercase();
+            if !"montuewedthufrisatsun".contains(&day) {
+                panic!("invalid day");
+            }
+            Reminder::new($crate::When::Day(day, $crate::Time::new($hour, $minute)), $sum, None)
+        }
+    };
+    ($day:ident $hour:literal:$minute:literal $sum:literal $body:literal) => {
+        {
+            let day = stringify!($day).to_lowercase();
+            if !"montuewedthufrisatsun".contains(&day) {
+                panic!("invalid day");
+            }
+            Reminder::new($crate::When::Day(day, $crate::Time::new($hour, $minute)), $sum, Some($body))
+        }
+    };
+    ($sec:literal $sum:literal) => {
+        Reminder::new($crate::When::Duration($sec), $sum, None)
+    };
+    ($sec:literal $sum:literal $body:literal) => {
+        Reminder::new($crate::When::Duration($sec), $sum, Some($body))
+    };
+    ($day:literal/$month:literal/$year:literal $hour:literal:$minute:literal $sum:literal) => {
+        Reminder::new($crate::When::Date($crate::Date::new($day, $month, $year), $crate::Time::new($hour, $minute)), $sum, None)
+    };
+    ($day:literal/$month:literal/$year:literal $hour:literal:$minute:literal $sum:literal $body:literal) => {
+        Reminder::new($crate::When::Date($day, $month, $year, $crate::Time::new($hour, $minute)), $sum, Some($body))
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,183 +1,7 @@
-use std::{thread, time, vec, fs::{self, File}, io, str::FromStr};
+use std::{thread, time, vec};
 
-use chrono::{DateTime, Local, Datelike, Timelike};
-use libnotify::Notification;
-
-#[derive(Debug)]
-struct Reminder<'a> {
-    summary: &'a str,
-    body: Option<&'a str>,
-    when: When,
-    done: bool,
-    notif: Notification,
-}
-
-#[derive(Debug, Clone)]
-struct Time {
-    hour: u32,
-    minutes: u32,
-}
-
-impl Time {
-    fn new(hour: u32, minutes: u32) -> Time {
-        Time { hour, minutes }
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Date {
-    day: u32,
-    month: u32,
-    year: i32,
-}
-
-impl Date {
-    fn new(day: u32, month: u32, year: i32) -> Date {
-        Date { day, month, year }
-    }
-}
-
-#[derive(Debug, Clone)]
-enum When {
-    Duration(u32),
-    Day(String, Time),
-    Date(Date, Time),
-    Time(Time),
-}
-
-impl<'a> Reminder<'a> {
-    pub fn new<'s>(when: When, summary: &'s str, body: Option<&'s str>) -> Reminder<'s> {
-        Reminder {
-            when,
-            summary,
-            body,
-            notif: Notification::new(summary, body, None),
-            done: false,
-        }
-    }
-
-    pub fn check(&mut self, now: DateTime<Local>) -> bool {
-        use When::*;
-        match self.when.clone() {
-            Duration(s) => {
-                if s > 0 {
-                    self.when = When::Duration(s - 1);
-                    false
-                } else {
-                    true
-                }
-            },
-            Day(d, t) => {
-                if d.to_lowercase() != now.weekday().to_string().to_lowercase() {
-                    return false;
-                }
-                if now.hour() < t.hour {
-                    return false;
-                }
-                if now.minute() < t.minutes {
-                    return false;
-                }
-                true
-            }
-            Date(d, t) => {
-                if now.year() < d.year && d.year != 0 {
-                    return false;
-                }
-                if now.month() < d.month {
-                    return false;
-                }
-                if now.day() < d.day {
-                    return false;
-                }
-                if now.hour() < t.hour {
-                    return false;
-                }
-                if now.minute() < t.minutes {
-                    return false;
-                }
-                true
-            },
-            Time(t) => {
-                if now.hour() < t.hour {
-                    return false;
-                }
-                if now.minute() < t.minutes {
-                    return false;
-                }
-                true
-            }
-        }
-    }
-
-    pub fn show(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        println!("{:?}", self);
-        self.done = true;
-        Ok(self.notif.show()?)
-    }
-}
-
-#[derive(Debug)]
-enum ReminderError<'a> {
-    FromStr(&'a str),
-}
-
-
-impl<'a> FromStr for Reminder<'a> {
-    type Err = ReminderError<'a>;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let v: Vec<&str> = s.split(";").collect();
-        if "montuewedthufrisatsun".contains(v[0]) {
-            todo!("parse days")
-        } else if v[0].contains("/") {
-            todo!("parse dates")
-        } else if v[0].contains(":") {
-            todo!("parse times")
-        } else {
-            todo!("parse durations")
-        }
-
-        Err(ReminderError::FromStr("unknown"))
-    }
-}
-
-macro_rules! remind {
-    ($hour:literal:$minute:literal $sum:literal) => {
-        Reminder::new(When::Time(Time::new($hour, $minute)), $sum, None)
-    };
-    ($hour:literal:$minute:literal $sum:literal $body:literal) => {
-        Reminder::new(When::Time(Time::new($hour, $minute)), $sum, Some($body))
-    };
-    ($day:ident $hour:literal:$minute:literal $sum:literal) => {
-        {
-            let day = stringify!($day).to_lowercase();
-            if !"montuewedthufrisatsun".contains(&day) {
-                panic!("invalid day");
-            }
-            Reminder::new(When::Day(day, Time::new($hour, $minute)), $sum, None)
-        }
-    };
-    ($day:ident $hour:literal:$minute:literal $sum:literal $body:literal) => {
-        {
-            let day = stringify!($day).to_lowercase();
-            if !"montuewedthufrisatsun".contains(&day) {
-                panic!("invalid day");
-            }
-            Reminder::new(When::Day(day, Time::new($hour, $minute)), $sum, Some($body))
-        }
-    };
-    ($sec:literal $sum:literal) => {
-        Reminder::new(When::Duration($sec), $sum, None)
-    };
-    ($sec:literal $sum:literal $body:literal) => {
-        Reminder::new(When::Duration($sec), $sum, Some($body))
-    };
-    ($day:literal/$month:literal/$year:literal $hour:literal:$minute:literal $sum:literal) => {
-        Reminder::new(When::Date(Date::new($day, $month, $year), Time::new($hour, $minute)), $sum, None)
-    };
-    ($day:literal/$month:literal/$year:literal $hour:literal:$minute:literal $sum:literal $body:literal) => {
-        Reminder::new(When::Date($day, $month, $year, Time::new($hour, $minute)), $sum, Some($body))
-    };
-}
+use chrono::Local;
+use remember::{Reminder, remind};
 
 fn main() {
     libnotify::init("Remember");
@@ -189,15 +13,14 @@ fn main() {
     let url = remind!(3 "<https://google.com>");                // Reminder::new(When::Duration(2), "url test", Some("<https://google.com>"));
 
     let mut reminders = vec![time, day, date, duration, url];
-    let mut len = reminders.len();
     let mut quit = false;
 
     while reminders.len() > 0 && !quit{
         thread::sleep(time::Duration::from_secs(1));
         let now = Local::now();
-        for i in 0..len {
+        for i in 0..reminders.len() {
             let mut r = &mut reminders[i];
-            if !r.done && r.check(now) {
+            if !r.is_done() && r.check(now) {
                 r.show().unwrap();
             }
         }
